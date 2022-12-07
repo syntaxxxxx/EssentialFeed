9685e9d3197e3e63c01ba2df3ef486c5e64edde4
fiqrihafzain <fiqrihafzainislami@gmail.com>
2022-12-08 05:53:52 +0700

    create FeedItem model

---
+++ FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Fiqri Hafzain Islami on 08/12/22.
+//
+
+import Foundation
+
+struct {
+    let id: UUID,
+    let description: String?
+    let location: String?
+    let imageUrl: URL
+}
